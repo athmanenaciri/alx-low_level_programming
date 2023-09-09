e061f82a5f389a94340487452e0b1f6b6030b261
root <naciriathmane@gmail.com>
2023-09-09 15:17:55 -0700

    fix betty coding style

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program.
+ *
+ * Description:
+ * This program prints a sequence of lowercase
+ *	letters excluding 'e' and 'q'.
+ * It starts with the letter 'a' and continues
+ *	until 24 characters are printed,
+ * avoiding 'e' and 'q'. The result is followed
+ *	by a newline character.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
 	char c;
+
 	i = 0;
 	c = 97;
 	while (i < 24)
 	{
-		if(c == 'e' || c == 'q')
+		if (c == 'e' || c == 'q')
 			c++;
 		putchar(c);
 		c++;
 		i++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
