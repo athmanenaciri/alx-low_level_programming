31edd42b3abbd19f9423011e57ad27803f027031
root <naciriathmane@gmail.com>
2023-09-09 15:33:55 -0700

    print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description:
+ * This program prints the lowercase
+ *	alphabet in reverse order,
+ * starting from 'z' and ending with 'a',
+ *	followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char ch;
+
+	i = 0;
+	ch = 122;
+	while (i < 26)
+	{
+		putchar(c);
+		c--;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
