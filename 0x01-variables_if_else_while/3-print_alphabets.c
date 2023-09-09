3f5c0517b0b71f3e276d03a36f7567012aa38b8c
root <naciriathmane@gmail.com>
2023-09-09 15:03:47 -0700

    adding description for main function

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program.
+ *
+ * Description:
+ * This program prints the lowercase letters from 'a' to 'z' and the uppercase
+ * letters from 'A' to 'Z' using two separate loops.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
         int i;
