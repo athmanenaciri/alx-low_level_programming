9247a182362f0c6b74a94d69bfb04fad825c69e8
root <naciriathmane@gmail.com>
2023-09-09 13:52:07 -0700

    adding description for main function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,16 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ * declare a variable type int named n
+ * //
+ * //
+ * check if n is equaled to 0 then print %d (the number) is zero followed by a new line (\n) using printf function
+ * if not equals to zero check if it's less print %d (the number) is negative followed by a new line (\n) using printf function 
+ * if not equals to zero or less then it is 100 % positive number so print %d (the number) is positive followed by a new line (\n) using printf function 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
