0a1ccd6ac11f0267a09c3280cac3c17344f30480
root <naciriathmane@gmail.com>
2023-09-09 15:20:48 -0700

    print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description:
+ * This program prints the string "0123456789"
+ *	followed by a newline character.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
