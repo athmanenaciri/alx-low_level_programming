bd8364c5366cc3145a2b7ad0fa2f7ff16f152b0c
root <naciriathmane@gmail.com>
2023-09-09 15:06:33 -0700

    betty coding style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,17 +10,17 @@
  */
 int main(void)
 {
-        int i;
-        char c;
+	int	i;
+	char	c;
 
-        i = 0;
-        c = 97;
-        while (i < 26)
-        {
-                putchar(c);
-                c++;
-                i++;
-        }
+	i = 0;
+	c = 97;
+	while (i < 26)
+	{
+		putchar(c);
+		c++;
+		i++;
+	}
 	i = 0;
 	c = 65;
         while (i < 26)
