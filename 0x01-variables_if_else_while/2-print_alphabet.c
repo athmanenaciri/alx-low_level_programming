4ffee356a247b62461296f36e4bd245f51d2de24
root <naciriathmane@gmail.com>
2023-09-09 14:57:35 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description:
+ * This program prints the lowercase letters from 'a' to 'z' using a loop.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	int i;
+	char c;
+
+	i = 0;
+	c = 97;
+	while(i < 26)
+	{
+		putchar(c);
+		c++;
+		i++;
+	}
+	putchar('\n');
+	return(0);
+}
