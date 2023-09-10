5b1c30569e2dcddd9f6553a990ae6dad5c936aaf
root <naciriathmane@gmail.com>
2023-09-09 17:51:48 -0700

    fix errors

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,29 +11,30 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
+	int a;
+	int b;
 
-	num1 = 0;
-	num2 = 1;
+	a = 0;
+	b = 1;
 
-	while (num1 <= 98)
+	while (a <= 98)
 	{
+		b = a + 1;
+		while(b <= 99)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
 			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-			if (num1 != 98 || num2 != 99)
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a != 98 || b != 99)
 			{
 				putchar(',');
 				putchar(' ');
 			}
-			num2++;
+			b++;
 		}
-		num1++;
-		num2 = num1 + 1;
+		a++;
 	}
 	putchar('\n');
 	return (0);
