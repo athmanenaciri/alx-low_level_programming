2b3ac509c2b3008728f23ab6fc3de1d46e627e4f
root <naciriathmane@gmail.com>
2023-09-12 20:09:13 -0700

    fibonacci Numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,16 +5,21 @@ int main(void)
 	int i;
 	int sum;
 	int j;
+	int tmp;
 
 	i = 0;
 	sum = 1;
-	j = 0;
-	while (i < 50)
+	j = 2;
+	printf("%d, %d, ", sum, j);
+	while (i < 48)
 	{
 		printf("%d", sum +j);
 		if (i != 49)
 			printf(", ");
-		j = sum;
+		tmp = sum;
+		sum = j;
+		j = tmp + j;
+
 		i++;
 	}
 	printf("\n");
