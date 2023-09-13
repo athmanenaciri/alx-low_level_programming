5330d1753e6fdad806307e49a07f0b68c8ad1daa
root <naciriathmane@gmail.com>
2023-09-12 18:27:00 -0700

    handel overflow case

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,17 @@
  * print_last_digit - Prints the last digit of a number
  * @num: The number to extract the last digit from
  *
- * Description: This function calculates and prints the last digit of the given integer 'num'.
+ * Description: This function calculates and prints
+ *	the last digit of the given integer 'num'.
  *
  * Return: The last digit of 'num'.
  */
 
 int print_last_digit(int num)
 {
-	if (num < 0)
+	if (num == INT_MIN)
+		return (_putchar(8 + '0'), 8);
+	else if (num < 0)
 		num *= -1;
 	_putchar((num % 10) + '0');
 	return (num % 10);
