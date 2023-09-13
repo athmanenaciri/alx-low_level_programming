7422ddc0ebfb2c5c81f297d23c56daff71655904
root <naciriathmane@gmail.com>
2023-09-12 19:50:55 -0700

    multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int main(void)
+{
+	int i;
+	int sum;
+
+	i = 0;
+	sum = 0;
+	while(i < 1024)
+	{
+		if(i / 3 == 0 || i / 5 == 0)
+			sum += i;
+		i++;
+	}
+	printf("%d\n", sum);
+	return(0);
+}
