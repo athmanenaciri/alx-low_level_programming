53f068af4bcee9436cbdc2a39814f077816f2910
Athmane Naciri <anaciri@e3r7p13.1337.ma>
2023-09-15 00:12:39 +0100

    i swear it's a square hdl

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * print_square - Prints a square of a specified size using the character '#'.
+ * @size: The size of the square.
+ *
+ * Description: If size is less than or equal to 0, it prints a new line.
+ */
 void print_square(int size)
 {
 	int i;
