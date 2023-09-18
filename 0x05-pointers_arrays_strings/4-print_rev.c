3182c9aad965a4d7f60dc333e47a7d7e11790fc1
athmane naciri <naciriathmane@gmail.com>
2023-09-19 00:09:29 +0100

    print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * print_rev - Prints a string in reverse followed by a new line.
+ * @s: A pointer to a string.
+ *
+ * Description: This function takes a string pointer and prints the characters
+ * of the string in reverse order, followed by a new line, to the standard output.
+ */
 
 void print_rev(char *s)
 {
