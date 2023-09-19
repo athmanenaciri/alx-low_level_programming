9a2f3984ed90ddd97676d9bd7ea68ad274b1eafa
Athmane Naciri <anaciri@e3r1p17.1337.ma>
2023-09-19 08:32:26 +0100

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,14 +14,13 @@ int _atoi(char *s)
 
 	result = 0;
 	sign = 1;
-    while (*s == ' ' || (*s >= 9 && *s <= 13))
-        s++;
-    while (*s == '-' || *s == '+')
-    {
+    while (*s == ' ' || (*s >= 9 && *s <= 13)
+		|| *s == '-' || *s == '+')
+	{
 		if (*s == '-')
         	sign = -1;
         s++;
-    }
+	}
     while (*s >= '0' && *s <= '9')
     {
         result = result * 10 + (*s - '0');
