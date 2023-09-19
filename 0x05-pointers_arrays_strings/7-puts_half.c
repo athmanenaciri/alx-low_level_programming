ddf12a1a084a7ac09baa880d980cc870ebd91aba
Athmane Naciri <anaciri@e3r11p7.1337.ma>
2023-09-19 04:02:41 +0100

    half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,22 +7,22 @@
  * Description: This function calculates the starting index based on whether
  * the length of the string is even or odd. If it's even, it starts printing
  * from the middle; if it's odd, it starts printing from the middle character.
- * The characters are printed until the end of the string, followed by a newline.
- */
+ * * The characters are printed until the end of the string, followed by a newline.
+ * */
 void puts_half(char *str)
 {
-    int length;
-    int start;
+	int length;
+	int start;
 
-    length = strlen(str);
-    if (length % 2 == 0)
-        start = length / 2;
-    else
-        start = (length + 1) / 2;
-    while (str[start] != '\0')
-    {
-        _putchar(str[start]);
-        start++;
-    }
-    _putchar('\n');
+	length = strlen(str);
+	if (length % 2 == 0)
+		start = length / 2;
+	else
+		start = (length + 1) / 2;
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+	_putchar('\n');
 }
