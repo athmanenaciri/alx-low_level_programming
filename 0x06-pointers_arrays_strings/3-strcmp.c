30df5d0dbf6947227d52e4b0103264d3350324eb
Athmane Naciri <anaciri@e3r3p12.1337.ma>
2023-09-21 21:05:26 +0100

    compares two strings and returns the difference between their first differing characters

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /*This is a C function that compares two 
- *	strings and returns the difference between
- *	their first differing characters.
- - If the strings are equal, it returns 0.
- Parameters:
-   - s1: The first string to compare
-   - s2: The second string to compare
- Returns:
-   - An integer representing the difference
-	between the first differing characters
-   */
+	strings and returns the difference between
+	their first differing characters.
+	- If the strings are equal, it returns 0.
+	Parameters:
+	- s1: The first string to compare
+	- s2: The second string to compare
+	Returns:
+	- An integer representing the difference
+	between the first differing characters */
 
 int _strcmp(char *s1, char *s2)
 {
