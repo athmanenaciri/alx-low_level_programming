22fa73199ffedb72a0fd8adb669148e528845dab
Athmane Naciri <anaciri@e3r7p11.1337.ma>
2023-09-27 01:02:03 +0100

    memset function implementation

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - Fills memory with a constant byte.
+ * @s: A pointer to the memory area to be filled.
+ * @b: The value (byte) to be set.
+ * @n: The number of bytes to be set to the value b.
+ *
+ * Return: A pointer to the memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i;
+
+    i = 0;
+    while ( i < n)
+    {
+        s[i] = b;
+        i++;
+    }
+    return (s);
+}
