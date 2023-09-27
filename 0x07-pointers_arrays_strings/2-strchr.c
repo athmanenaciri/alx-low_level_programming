8b50454924dd011d786e41c48bd82eacae719f59
Athmane Naciri <anaciri@e3r7p11.1337.ma>
2023-09-27 01:12:44 +0100

    strchr function implementation

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,16 @@
  */
 char    *_strchr(char *s, char c)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] == c)
-            return (&s[i]);
-        i++;
-    }
-    if (s[i] == c)
-        return (s);
-    return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	if (s[i] == c)
+		return (s);
+	return (NULL);
 }
