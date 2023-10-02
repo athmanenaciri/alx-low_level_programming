221c71bba63cd3f83557c11f11cc2c24f106cb46
Athmane Naciri <anaciri@e3r7p11.1337.ma>
2023-10-02 02:56:08 +0100

    fix betty style

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,12 +4,11 @@
  * _print_rev_recursion - Prints a string in reverse recursively.
  * @s: The string to be printed in reverse.
  */
+
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0') /* Base case: End of string */
-        return;
-
-    _print_rev_recursion(s + 1); /* Recursive call to print the rest of the string in reverse */
-
-    _putchar(*s); /* Print the current character */
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
