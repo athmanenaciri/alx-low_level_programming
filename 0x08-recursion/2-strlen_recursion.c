77ce3f5881845b5335764e43a8c2ffb309c8dc55
Athmane Naciri <anaciri@e3r7p11.1337.ma>
2023-10-02 03:04:29 +0100

    add func description [betty style]

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of a string s.
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to find the length of.
+ *
+ * Description: This function recursively calculates the length of a string by
+ * counting each character until it reaches the null terminator.
+ *
+ * Return: The length of the string.
  */
-
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
