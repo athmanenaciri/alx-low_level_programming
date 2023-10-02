65b6525eb6f953bfd78a9327a4a8f4c7f020304a
Athmane Naciri <anaciri@e3r7p11.1337.ma>
2023-10-02 03:36:32 +0100

    is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * is_prime_recursive - Helper function to check for prime.
+ * @n: The number to be checked.
+ * @divisor: The current divisor to check.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+
+int is_prime_recursive(int n, int divisor)
+{
+	if (divisor >= n)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (is_prime_recursive(n, divisor + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to be checked.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime_recursive(n, 2));
+}
