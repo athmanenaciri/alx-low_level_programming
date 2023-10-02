87b6220b318e203c7f7b2e8eb16cf21dfe882da0
athmane naciri <naciriathmane@gmail.com>
2023-10-02 22:52:17 +0100

    new func protoype

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * this program print's all the arguments including the executable.
+ * using two loops, to loop around the arguments
+ * and the second one to loop throught charachters .
+ */
+
+int main(int ac, char **av)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			write(1, &av[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
