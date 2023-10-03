07315f78809776841db6089cebd27788691c1d3f
athmane naciri <naciriathmane@gmail.com>
2023-10-03 15:41:24 +0100

    new func protoype

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void	_putchar(char	c);
+char *create_array(unsigned int size, char c);
+
+#endif
