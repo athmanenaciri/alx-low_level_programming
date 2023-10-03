31745d75d12ebd348f450a5269f3e0c7a9c13035
Athmane Naciri <anaciri@e3r8p5.1337.ma>
2023-10-04 00:50:55 +0100

    new func prototype

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,15 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
-
-
 void	_putchar(char	c);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
 
 #endif
