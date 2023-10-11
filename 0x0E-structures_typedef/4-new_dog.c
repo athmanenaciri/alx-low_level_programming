b76339d0e297c4deef43f485b68c983cc54d7275
Athmane Naciri <anaciri@e1r2p13.1337.ma>
2023-10-12 00:34:01 +0100

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,26 +9,26 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t	*new_dog;
-    char	*name_copy;
+	dog_t	*new_dog;
+	char	*name_copy;
 	char	*owner_copy;
 
-    if (!name || !owner || age < 0)
-        return (NULL);
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
-    name_copy = strdup(name);
-    owner_copy = strdup(owner);
-    if (name_copy == NULL || owner_copy == NULL)
-    {
-        free(new_dog);
-        free(name_copy);
-        free(owner_copy);
-        return (NULL);
-    }
-    new_dog->name = name_copy;
-    new_dog->age = age;
-    new_dog->owner = owner_copy;
-    return (new_dog);
+	if (!name || !owner || age < 0)
+		return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	name_copy = strdup(name);
+	owner_copy = strdup(owner);
+	if (name_copy == NULL || owner_copy == NULL)
+	{
+		free(new_dog);
+		free(name_copy);
+		free(owner_copy);
+		return (NULL);
+	}
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+	return (new_dog);
 }
