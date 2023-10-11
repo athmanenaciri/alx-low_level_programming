2ab797e656efc4010488a0fa95d715a9255d86d3
Athmane Naciri <anaciri@e1r7p10.1337.ma>
2023-10-11 18:30:37 +0100

    a program that prints the name of the file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
-    * main - prints the name of the file it was compiled from
-    *
-    * Return: Always 0.
-*/
+/**
+ * main - Prints the name of the file it was compiled from.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
     printf(__FILE__ "\n");
