8bd63fbd13ca5e862b1660770b7f31c219f6fed0
Athmane Naciri <anaciri@e1r2p13.1337.ma>
2023-10-12 00:39:16 +0100

    a function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - Frees memory allocated for a dog structure.
+ * @d: Pointer to the dog structure to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
+
