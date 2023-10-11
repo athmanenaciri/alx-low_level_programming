840d2719fe9574fba2d687a1fa4ef83d98d5c112
Athmane Naciri <anaciri@e1r2p13.1337.ma>
2023-10-12 00:24:17 +0100

    Write a function that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,45 +1,47 @@
 #include "dog.h"
+#include <stdio.h>
 
-/*
- * _putstr function uses write 
- *	system call to write the string
- *	char by char at the stdout 
+/**
+ * _putstr - Custom function to print a string
+ * @str: The string to be printed
  */
-void	_putstr(char	*str)
+void _putstr(char *str)
 {
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
+    if (str)
+    {
+        while (*str)
+        {
+            putchar(*str);
+            str++;
+        }
+    }
+    else
+    {
+        printf("(nil)");
+    }
 }
 
-/*
+/**
  * print_dog - Print the attributes of a dog structure
  * @d: Pointer to the dog structure to be printed
- *
- * Description: This function prints the attributes of a dog structure,
- * including its name, age, and owner. If any of these attributes is NULL,
- * it prints "(nil)" as a placeholder. The function handles the case where
- * 'd' is NULL, in which case it does nothing.
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	if (!d->name)
-		printf("Name: (nil)\n");
-	else 
-		printf("Name: %s\n", d->name);
-	if (!d->age)
-		printf("Age: (nil)\n");
-	else 
-		printf("Age: %f\n", d->age);
-	if (!d->owner)
-		printf("Owner: (nil)");
-	else 
-		printf("Owner: %s", d->owner);
+    if (d == NULL)
+        return;
+
+    printf("Name: ");
+    _putstr(d->name);
+
+    printf("\nAge: ");
+    if (d->age < 0)
+        printf("(nil)");
+    else
+        printf("%f", d->age);
+
+    printf("\nOwner: ");
+    _putstr(d->owner);
+    
+    printf("\n");
 }
+
