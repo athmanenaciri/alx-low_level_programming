f28e13b61482ae2e31c93e5c16fecf65891234e9
Athmane Naciri <anaciri@e2r6p14.1337.ma>
2023-10-21 01:47:35 +0100

    header file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * struct list_s - Singly linked list structure
+ * @str: String (node data)
+ * @len: Length of the string
+ * @next: Points to the next node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+#endif
