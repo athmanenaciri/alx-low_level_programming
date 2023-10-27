169fb29e814fbcc28f86f35603a1cc6ac5955565
Athmane Naciri <anaciri@e1r1p5.1337.ma>
2023-10-27 02:14:53 +0100

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void	before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - Prints the desired message before main is executed.
+ */
+void	before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
