f94d2dcebc01b9068e5519df71eb6a2ebc0230c4
Athmane Naciri <anaciri@e1r2p1.1337.ma>
2023-10-30 14:19:11 +0100

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,17 +8,16 @@
  *
  * Return: 1 on success, -1 on failure.
  */
-int create_file(const char *filename, char *text_content)
+int	create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int len;
-	int n_written;
+	int	fd;
+	int	len;
+	int	n_written;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT
-			| O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
