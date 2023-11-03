6e41eab8065da4aa3bb7d47c22e0941e689f6166
Athmane Naciri <anaciri@e1r1p3.1337.ma>
2023-11-04 00:17:09 +0100

    tryinf

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define BUFFER_SIZE 1024
 
@@ -14,55 +16,55 @@
  */
 int main(int ac, char *av[])
 {
-    int fd_from, fd_to, bytes_read, bytes_written;
-    char buffer[BUFFER_SIZE];
+	int fd_from, fd_to, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s file_from file_to\n", av[0]);
-        exit(97);
-    }
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
 
-    fd_from = open(av[1], O_RDONLY);
-    if (fd_from == -1)
-    {
-        dprintf(2, "Error: Can't read from %s\n", av[1]);
-        exit(98);
-    }
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from %s\n", av[1]);
+		exit(98);
+	}
 
-    fd_to = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-    if (fd_to == -1)
-    {
-        dprintf(2, "Error: Can't write to %s\n", av[2]);
-        exit(99);
-    }
+	fd_to = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 
-    while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-    {
-        bytes_written = write(fd_to, buffer, bytes_read);
-        if (bytes_written == -1 || bytes_read != bytes_written)
-        {
-            dprintf(2, "Error: Can't write to %s\n", av[2]);
-            exit(99);
-        }
-    }
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_read != bytes_written)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
 
-    if (bytes_read == -1)
-    {
-        dprintf(2, "Error: Can't read from %s\n", av[1]);
-        exit(98);
-    }
+	if (bytes_read == -1)
+	{
+		dprintf(2, "Error: Can't read from %s\n", av[1]);
+		exit(98);
+	}
 
-    if (close(fd_from) == -1)
-    {
-        dprintf(2, "Error: Can't close fd %d\n", fd_from);
-        exit(100);
-    }
+	if (close(fd_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
 
-    if (close(fd_to) == -1)
-    {
-        dprintf(2, "Error: Can't close fd %d\n", fd_to);
-        exit(100);
-    }
-    return (0);
+	if (close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	return (0);
 }
